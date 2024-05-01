518c9655920a101676b5904ebd9a4426ce68bffa
Vishnu S <141705372+Vishnu252005@users.noreply.github.com>
2024-05-01 17:49:31 +0530

    soln

--- components/form/recipe-form.tsx
+++ components/form/recipe-form.tsx
@@ -1,11 +1,9 @@
-"use client"
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import React, { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-
-import { defaultValues, formSchema, type FormData } from "@/types/types"
-import { Button } from "@/components/ui/button"
+import { defaultValues, formSchema, type FormData } from "@/types/types";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,30 +11,26 @@ import {
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Slider, SliderThumb } from "@/components/ui/slider"
-import { RecipeFormLabel } from "@/components/form/label-form-field"
-import {
-  options,
-  RadioGroupFormField,
-} from "@/components/form/radio-group-form-field"
-import { SelectFormField } from "@/components/form/select-form-field"
-import { SwitchFormField } from "@/components/form/switch-form-field"
-import { Icons } from "@/components/icons"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RecipeFormLabel } from "@/components/form/label-form-field";
+import { RadioGroupFormField } from "@/components/form/radio-group-form-field";
+import { SelectFormField } from "@/components/form/select-form-field";
+import { SwitchFormField } from "@/components/form/switch-form-field";
+import { Icons } from "@/components/icons";
 
 interface RecipeFormProps {
-  onSubmit: (values: FormData, e: React.FormEvent) => void
-  isLoading: boolean
+  onSubmit: (values: FormData, e: React.FormEvent) => void;
+  isLoading: boolean;
 }
 
 export function RecipeForm({ onSubmit, isLoading }: RecipeFormProps) {
-  const [showAdditionalFields, setShowAdditionalFields] = useState(false)
+  const [showAdditionalFields, setShowAdditionalFields] = useState(false);
 
   const form = useForm<FormData>({
     resolver: zodResolver(formSchema),
     defaultValues,
-  })
+  });
 
   return (
     <Form {...form}>
@@ -55,7 +49,7 @@ export function RecipeForm({ onSubmit, isLoading }: RecipeFormProps) {
               <FormControl>
                 <div className="relative">
                   <Input
-                    placeholder="Bread , Arduino Board , Jumper Wires"
+                    placeholder="Breadboard, Arduino Board, Jumper Wires"
                     {...field}
                     onClick={() => setShowAdditionalFields(true)}
                     className="rounded-xl bg-primary text-secondary shadow-lg placeholder:text-secondary/70"
@@ -69,50 +63,25 @@ export function RecipeForm({ onSubmit, isLoading }: RecipeFormProps) {
         />
         {showAdditionalFields && (
           <>
-            <FormField
-              control={form.control}
-              name="cooking_time"
-              render={({ field }) => (
-                <FormItem className="space-y-3">
-                  <RecipeFormLabel
-                    stepIndex="2"
-                    labelIndex="How much time can you spare?"
-                  />
-                  <FormControl>
-                    <Slider
-                      id="cooking-time"
-                      aria-label="Choose cooking time"
-                      defaultValue={[5]}
-                      max={120}
-                      step={10}
-                      min={5}
-                      onValueChange={field.onChange}
-                      {...field}
-                    >
-                      <SliderThumb aria-label="Cooking time"></SliderThumb>
-                    </Slider>
-                  </FormControl>
-                  <FormDescription className="flex flex-row-reverse">
-                    🕛 {field.value} minutes
-                  </FormDescription>
-                </FormItem>
-              )}
-            />
             <FormItem>
               <RecipeFormLabel
                 stepIndex="3"
-                labelIndex="How many hungry souls?"
+                labelIndex="How many will you spark?"
               />
               <RadioGroupFormField
                 form={form}
                 name="people"
-                options={options}
+                options={[
+                  { label: "One", value: "one" },
+                  { label: "A few", value: "few" },
+                  { label: "Many", value: "many" },
+                ]}
               />
             </FormItem>
             <FormItem>
               <RecipeFormLabel
                 stepIndex="4"
-                labelIndex="Are you the master of the kitchen domain?"
+                labelIndex="Are you the master of the electronics domain?"
               />
               <SelectFormField form={form} name="difficulty" />
             </FormItem>
@@ -121,31 +90,31 @@ export function RecipeForm({ onSubmit, isLoading }: RecipeFormProps) {
                 stepIndex="5"
                 labelIndex="Any specific preferences for your feast?"
               />
-              <SwitchFormField
+              {/* <SwitchFormField
                 form={form}
                 name="low_calori"
                 label="⚖️ Low Cal"
               />
               <SwitchFormField form={form} name="vegan" label="🌿 Vegan" />
-              <SwitchFormField form={form} name="paleo" label="🍖 Paleo" />
+              <SwitchFormField form={form} name="paleo" label="🍖 Paleo" /> */}
             </FormItem>
-            {isLoading ? (
-              <Button disabled size="lg" className="w-full font-semibold">
-                <Icons.loader
-                  className="mr-2 size-4 animate-spin"
-                  aria-hidden="true"
-                />
-                Generating recipe
-              </Button>
-            ) : (
-              <Button type="submit" size="lg" className="w-full font-semibold">
-                Generate recipe
-                <Icons.generate className="ml-2 size-4" aria-hidden="true" />
-              </Button>
-            )}{" "}
           </>
+        )}
+        {isLoading ? (
+          <Button disabled size="lg" className="w-full font-semibold">
+            <Icons.loader
+              className="mr-2 size-4 animate-spin"
+              aria-hidden="true"
+            />
+            Generating ideas
+          </Button>
+        ) : (
+          <Button type="submit" size="lg" className="w-full font-semibold">
+            Generate ideas
+            <Icons.generate className="ml-2 size-4" aria-hidden="true" />
+          </Button>
         )}
       </form>
     </Form>
-  )
+  );
 }
